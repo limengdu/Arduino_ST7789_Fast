a7f4c74f39a101154d3744424d711b4bd4980218
Mengdu <747632169@qq.com>
2025-08-21 16:27:40 +0800

    Fix bug

--- Arduino_ST7789_Fast.h
+++ Arduino_ST7789_Fast.h
@@ -20,10 +20,10 @@
 #define FAST_IO     1
 
 #if FAST_IO
-#define    LCD_SCK_SET    gpio_put(24, 1);           // SET SCK HIGH
-#define    LCD_SDA_SET    gpio_put(23, 1);           // SET SDA HIGH
-#define    LCD_SCK_CLR    gpio_put(24, 0);          // SET SCK LOW
-#define    LCD_SDA_CLR    gpio_put(23, 0);          // SET SDA LOW
+#define    LCD_SCK_SET    digitalWrite(pinSck, HIGH);           // SET SCK HIGH
+#define    LCD_SDA_SET    digitalWrite(pinSda, HIGH);           // SET SDA HIGH
+#define    LCD_SCK_CLR    digitalWrite(pinSck, LOW);          // SET SCK LOW
+#define    LCD_SDA_CLR    digitalWrite(pinSda, LOW);          // SET SDA LOW
 #endif
 
 #define ST7789_TFTWIDTH         240
